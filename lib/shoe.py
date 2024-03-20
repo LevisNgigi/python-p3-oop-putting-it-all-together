caff9ade1ac888e42d1df2ad6ab355dd64d34651
Levis Ngigi <ngigilevis@gmail.com>
2024-03-20 09:37:18 +0300

    update code

--- lib/shoe.py
+++ lib/shoe.py
@@ -3,11 +3,19 @@
 class Shoe:
     def __init__(self, brand, size):
         self.brand = brand
-        if not isinstance(size, int):
-            raise ValueError("size must be an integer")
         self.size = size
-        self.condition = "Used"
+    
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else: 
+            print('size must be an integer')
 
     def cobble(self):
-        self.condition = "New"
-        print("Your shoe is as good as new!")
+        print('Your shoe is as good as new!')
+        Shoe.condition = "New"
+
+    size = property(get_size, set_size)
